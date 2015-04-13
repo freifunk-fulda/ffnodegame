a3d955727183a3addacbd571316c0297dfc19d58
root <root@fffd-srv1.freifunk-fulda.de>
2015-04-13 11:46:30 +0200

    updated configuration

--- settings.rb
+++ settings.rb
@@ -10,26 +10,26 @@
 #
 
 #source path of node data
-JSONSRC='http://burgtor.ffhl/mesh/nodes.json'
+JSONSRC='http://map.fffd/nodes.html'
 
 #password for commands over GET requests
 PWD='hackme'
 
 #score values
 SC_OFFLINE=-100
-SC_GATEWAY=100
+SC_GATEWAY=0
 SC_PERCLIENT=25
 SC_PERVPN=10 #divided by quality
 SC_PERMESH=50 #divided by quality
 
 #fun option - penalty for Apple devices connected
-PUNISHAPPLE=true
+PUNISHAPPLE=false
 SC_PERAPPLE=-15
 
 #----
 
 #hide following nodes from scores
-BLACKLIST=['burgtor','holstentor','muehlentor']
+BLACKLIST=['gw01', 'gw02', 'fffd-srv1', 'fffd-srv2', 'fffd-nms']
 
 #----
 
